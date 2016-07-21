fe2290ee27c4c1e66dc914ecfff0877ccea3366a
Cody Scholberg <cscholberg@gmail.com>
2016-07-21 13:35:13 -0400

    audio control commit

--- client/js/HomeCtrl.js
+++ client/js/HomeCtrl.js
@@ -6,5 +6,8 @@ angular.module('pundexApp').controller('homeController', ['$scope', function($sc
     $('#title').prepend('<br><h3><em>A Play On Words...</em></h3><p style="font-size: 1em;"><em>by</em></p><img class="center-block img-fluid" src="client/img/title2.png" id="title">');
     $('#mid').prepend('<img class="center-block img-fluid" src="client/img/jump.gif">');
     $('#theme')[0].play();
+    $('#theme').show();
+    var aud = document.getElementById('theme');
+    aud.volume = 0.4;
   });
 }]);
